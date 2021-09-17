5b0f6f022000ac93eca070fdbf21f8cb3fb5832e
= <=>
2021-09-17 11:38:00 +0700

    Merge remote-tracking branch 'github/master' into master
    # Conflicts:
    #	src/main/java/DAO/AbsDAO.java
    #	src/main/java/DAO/MovieDAO.java
    #	src/main/java/application/MyApplication.java
    #	src/main/java/controller/CommentController.java
    #	src/main/java/controller/HomeController.java
    #	src/main/java/controller/MovieController.java
    #	src/main/java/controller/MyController.java
    #	src/main/java/filter/MyFilter.java
    #	src/main/java/model/Comment.java
    #	src/main/java/model/Critic.java
    #	src/main/java/model/Imdb.java
    #	src/main/java/model/Movie.java
    #	src/main/java/model/Tomatoes.java
    #	src/main/java/service/CommentService.java
    #	src/main/java/service/MovieService.java
    #	src/main/java/utils/MyTime.java
    #	src/main/webapp/css/main.css
    #	src/main/webapp/images/user.svg
    #	src/main/webapp/js/main.js
    #	src/main/webapp/templates/fragments/frontend.html
    #	src/main/webapp/templates/fragments/frontend.properties
    #	src/main/webapp/templates/fragments/frontend_fr.properties
    	src/main/webapp/templates/fragments/frontend_vi.properties
    #	src/main/webapp/templates/index.html
    #	src/main/webapp/templates/movie.html

--- src/main/java/model/Comment.java
+++ src/main/java/model/Comment.java
@@ -4,35 +4,18 @@ import lombok.Data;
 import org.bson.types.ObjectId;
 import utils.MyTime;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> github/master
 import java.util.Date;
 
 @Data
 public class Comment {
     private ObjectId id;
-<<<<<<< HEAD
-
-    private String text;
-    private Date date;
-    private String email;
-    private String name;
-    private ObjectId movie_id;
-=======
     private String name;
     private String email;
     private String text;
     private ObjectId movie_id;
     private Date date;
->>>>>>> github/master
 
     public String getTimeAgo() {
         return MyTime.timeAgo(new Date(), date);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> github/master
